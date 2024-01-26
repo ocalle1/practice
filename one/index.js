c09e03d1412e41c726faec239f7bbfc023acc70e
Oscar Calle <ocalle374@gmail.com>
2024-01-26 15:14:57 -0500

    practice

--- one/index.js
+++ one/index.js
@@ -28,3 +28,20 @@ function getAge(inputString){
       // Return the extracted age as an integer
       return age
     }
+
+    // 3. Given an array a that contains only numbers in the range from 1 to a.length, find the first duplicate number for which the second occurrence has the minimal index. In other words, if there are more than 1 duplicated numbers, return the number for which the second occurrence has a smaller index than the second occurrence of the other number does. If there are no such elements, return -1.
+    function solution(a) {
+      // create a set to store seen elements
+    const seen = new Set();
+    
+    for(let i = 0; i < a.length; i++) {
+      // if element is already in the set, return it
+      if(seen.has(a[i])) {
+        return a[i]
+      }
+      //Add element to the set
+      seen.add(a[i])
+    }
+    // if no duplicate is found, return -1
+    return -1;
+    }
