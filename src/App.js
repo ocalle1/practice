704ad2521a9acaf9dbbc3cf0a62b44cbce0b6c6f
Oscar Calle <ocalle374@gmail.com>
2024-12-15 15:26:30 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Accordian from './components/accordian';
+
+
+function App() {
+  return (
+  <>
+   <div className='App'>
+    {/* Accordian component */}
+    <Accordian />
+           </div>
+  </>
+  );
+}
+
+export default App;
---
+++ src/components/accordian/data.js
@@ -0,0 +1,11 @@
+
+
+const data = [
+    { id: '1', question: 'What are accordion components?', answer: 'Accordion components are user interface elements used for organizing and presenting...' },
+    { id: '2', question: 'What are they used for?', answer: 'They are commonly employed in various contexts, including FAQs, product descriptions...' },
+    { id: '3', question: 'Accordion as a musical instrument', answer: 'The accordion is a musical instrument with a keyboard and bellows. It produces sound...' },
+    { id: '4', question: 'Can I create an accordian component with a different framework?', answer: 'Can I' }
+  ];
+  
+  export default data;  // Default export
+  
