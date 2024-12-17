9161106ab12c070c60b32893c960157bde149c9f
Oscar Calle <ocalle374@gmail.com>
2024-12-16 22:09:56 -0600

    added ternary operator to switch between the 2 functions - handlemultiSelection and handSinlgeSelection

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -8,25 +8,43 @@ import './styles.css'
 export default function Accordian() {
 
     const [selected, setSelected] = useState(null)
+    // Multiple items can be selected
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+    // When multiple items are selected this is where they are stored
+    const [multiple, setMultiple] = useState([])
 
     function handleSingleSelection(getCurrentId) {
-        //This line toggles the selected  item, if an item is already selected then the item is deselected(nuill)
+        //toggles the selected  item, if an item is already selected then the item is deselected(nuill)
         setSelected(getCurrentId === selected ? null : getCurrentId);
     }
+
+    function handleMultiSelection(getCurrentId) {
+
+    }
+
     console.log(selected);
     return <div className='wrapper'>
-    <button>Enable Multi Selection</button>
+        {/* when button clicked the enableMultiSelecction becomes True */}
+        <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className='accordian'>
-            
+
             {/* the data && data.length is a conditional statement which is conbined by the &&. data = ensures that the data is truthy and data.length>0 ensures that the array is not empty;
             if not empty the ?(ternary) is used to handle both scenariious based on whether the data is valid and contains elements */}
             {/*  data.map - if the above is truthy then then the .map method iterates over the items in data and renders them dynamically */}
             {
-                
+
                 data && data.length > 0 ?
-                
+
                     data.map(dataItem => <div className='item'>
-                        <div onClick={() => handleSingleSelection(dataItem.id)} className='title'>
+                    {/* added the ternary operator to switch between the 2 functions
+                    if enableMultiSelection is True then handleMultiSelection(dataItem.id) is called and if False handleSingleSelection(dataItem.id) is called */}
+                        <div onClick={
+                            enableMultiSelection
+                            ? () => handleMultiSelection(dataItem.id)
+                            : () => handleSingleSelection(dataItem.id)
+                            } 
+                            className='title'
+                            >
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
