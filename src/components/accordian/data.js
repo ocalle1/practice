448b272c09ccc48d57d69ddfa04a5febbea7d8b7
Oscar Calle <ocalle374@gmail.com>
2024-12-21 15:47:05 -0600

    updated comments

--- src/components/accordian/data.js
+++ src/components/accordian/data.js
@@ -4,7 +4,7 @@ const data = [
     { id: '1', question: 'What are accordion components?', answer: 'Accordion components are user interface elements used for organizing and presenting...' },
     { id: '2', question: 'What are they used for?', answer: 'They are commonly employed in various contexts, including FAQs, product descriptions...' },
     { id: '3', question: 'Accordion as a musical instrument', answer: 'The accordion is a musical instrument with a keyboard and bellows. It produces sound...' },
-    { id: '4', question: 'Can I create an accordian component with a different framework?', answer: 'Can I' }
+    { id: '4', question: 'Can I create an accordian component with a different framework?', answer: 'Yes, it is possible to create an accordian component with another framework' }
   ];
   
   export default data;  // Default export
--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -6,55 +6,73 @@ import data from './data';
 import './styles.css'
 
 export default function Accordian() {
-
+    //A state which tracks a single selected item. it can either store an ID or null if no item is selected 
     const [selected, setSelected] = useState(null)
-    // Multiple items can be selected
+    //a boolean state that toggles whether multiple selection is enabled or not. when true, user can select multiple items
     const [enableMultiSelection, setEnableMultiSelection] = useState(false)
-    // When multiple items are selected this is where they are stored
+    //An array where multiple selected ID are stored
     const [multiple, setMultiple] = useState([])
 
+    //Single selection mode
     function handleSingleSelection(getCurrentId) {
         //toggles the selected  item, if an item is already selected then the item is deselected(nuill)
         setSelected(getCurrentId === selected ? null : getCurrentId);
     }
 
+    //Multiple seclection mode
     function handleMultiSelection(getCurrentId) {
+        //Creates a shallow copy of the multiple array(list of selected IDs), ensuring that any changes made to the cpyMultiple does NOT affect multiple array  
+        let cpyMultiple = [...multiple];
+        // the method  indexOf looks for the index of getCurrentId in the cpyMultiple array. 
 
+        const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId)
+        console.group(findIndexOfCurrentId);
+        //  The findIndexOfCurrentId holds the result of searching for getCurrentId in the cpyMultiple array.
+        //findIndexOfCurrentId === -1, means the getCurrentId was not found in the array (cpyMultiple).
+        // When getCurrentIf is not found, its added to the array with the push method
+        if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId)
+        // if the getCurrentId is already in the array, it gets removed using .splice()
+        else cpyMultiple.splice(findIndexOfCurrentId, 1)
+        // updates the state
+        setMultiple(cpyMultiple)
     }
 
-    console.log(selected);
+    console.log(selected, multiple);
     return <div className='wrapper'>
-        {/* when button clicked the enableMultiSelecction becomes True */}
+        {/* button toggles the value of enableMultiSelection(single and multiple selection mode) */}
         <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>Enable Multi Selection</button>
         <div className='accordian'>
 
-            {/* the data && data.length is a conditional statement which is conbined by the &&. data = ensures that the data is truthy and data.length>0 ensures that the array is not empty;
-            if not empty the ?(ternary) is used to handle both scenariious based on whether the data is valid and contains elements */}
-            {/*  data.map - if the above is truthy then then the .map method iterates over the items in data and renders them dynamically */}
+            {/*The data maps over the data array to dynamically render each item */}
+            {/*Each dataItem has a title (dataItem.question) and, if selected, displays its correspoinding answer(dataItem.answer)  */}
+            {/*  */}
             {
-
                 data && data.length > 0 ?
 
                     data.map(dataItem => <div className='item'>
-                    {/* added the ternary operator to switch between the 2 functions
+                        {/* added the ternary operator to switch between the 2 functions
                     if enableMultiSelection is True then handleMultiSelection(dataItem.id) is called and if False handleSingleSelection(dataItem.id) is called */}
                         <div onClick={
                             enableMultiSelection
-                            ? () => handleMultiSelection(dataItem.id)
-                            : () => handleSingleSelection(dataItem.id)
-                            } 
+                                ? () => handleMultiSelection(dataItem.id)
+                                : () => handleSingleSelection(dataItem.id)
+                        }
                             className='title'
-                            >
+                        >
                             <h3>{dataItem.question}</h3>
                             <span>+</span>
                         </div>
-                        {/* selected is State which is a built-in object that allows components to store and manage data that change over time 
-                        When the event handler  onClick={() => handleSingleSelection(dataItem.id)} is clicked the State(selected) evaluates that the dataItem.id element exists and then it renders the JSX {dataItem.answer}*/}
+                        {/* Ternary operator(if-else statement) 
+                        if enableMultiSelection is True then the first part of the operator is executed(multiple selection mode)
+                        if false then the second part is executed(single selection mode)
+                        */}
                         {
-                            selected === dataItem.id ?
-                                <div className='content'>{dataItem.answer}</div>
-                                : null
+                            enableMultiSelection ?
+                                multiple.indexOf(dataItem.id) !== -1 &&
+                                <div className='content'>{dataItem.answer}</div> :
+                                selected === dataItem.id && <div className='content'>{dataItem.answer}</div>
                         }
+                        
                     </div>
                     )
                     : <div>No data found!</div>
